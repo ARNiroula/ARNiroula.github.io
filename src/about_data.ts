8c9cb050fb88f1d4b00c823b0d5fb26d56c5b6d2
Anup Raj Niroula <a.r.niroula@gmail.com>
2025-07-09 02:47:13 -0400

    Add blog components

---
+++ src/about_data.ts
@@ -0,0 +1,42 @@
+export const jobs = [
+    {
+        "title": "AI Engineer",
+        "description": "AI Stuff",
+        "company": "Medlaunch Concepts",
+        "url": "https://medlaunchlp.com/",
+        "startDate": "2025",
+        "endDate": "Present",
+        "techs": ["FastAPI", "AWS", "S3"]
+    },
+    {
+        "title": "Software Engineer",
+        "description": "R programming Stuff",
+        "company": "Nimble Clincal Research",
+        "url": "https://nimble-cr.com/",
+        "startDate": "2021",
+        "endDate": "2023",
+        "techs": ["FastAPI", "R Programming", "Docker", "Django"]
+    },
+]
+
+
+export const edu = [
+    {
+        "title": "Computer Engineering",
+        "degree": "M.S. Engineering",
+        "startDate": "2024",
+        "endDate": "2026",
+        "university": "New York University",
+        "gpa": "4.0",
+        "url": "https://www.nyu.edu/",
+    },
+    {
+        "title": "Computer Engineering",
+        "degree": "B.E. Engineering",
+        "startDate": "2017",
+        "endDate": "2022",
+        "university": "Kathmandu University",
+        "gpa": "3.83",
+        "url": "https://ku.edu.np/",
+    },
+]
---
+++ src/content/blog/post-1.md
@@ -0,0 +1,8 @@
+---
+title: 'Publishing this website to github pages'
+description: 'Using Github Pages and Action to deploy this personal website'
+pubDate: 'Jul 09 2025'
+tags: ["astro", "github", "deployment"]
+---
+
+Well, hello there! Welcome to my blog, it'll be a confusing journey to say the least!
