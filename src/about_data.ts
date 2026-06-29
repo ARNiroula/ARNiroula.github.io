f8c03037ad7ba2a77dd195b8ad7f34db9fc3305a
Anup Raj Niroula <a.r.niroula@gmail.com>
2026-06-29 17:31:19 -0400

    Update About me and Header

--- src/about_data.ts
+++ src/about_data.ts
@@ -1,11 +1,29 @@
 export const jobs = [
     {
+        "title": "ML/AI Engineer",
+        "description": "ML/AI Stuff",
+        "company": "Medlaunch Concepts",
+        "url": "https://medlaunchlp.com/",
+        "startDate": "Jan, 2026",
+        "endDate": "May, 2026",
+        "techs": ["FastAPI", "AWS", "Claude Agent SDK", "Agentic AI Development"]
+    },
+    {
+        "title": "Graduate Teaching Assistant",
+        "description": "Classical ML stuff",
+        "company": "New York University",
+        "url": "https://www.nyu.edu/",
+        "startDate": "Jan, 2025",
+        "endDate": "Dec, 2025",
+        "techs": ["FastAPI", "AWS", "S3"]
+    },
+    {
         "title": "AI Engineer",
         "description": "AI Stuff",
         "company": "Medlaunch Concepts",
         "url": "https://medlaunchlp.com/",
-        "startDate": "2025",
-        "endDate": "Present",
+        "startDate": "May, 2025",
+        "endDate": "Aug, 2025",
         "techs": ["FastAPI", "AWS", "S3"]
     },
     {
@@ -13,8 +31,8 @@ export const jobs = [
         "description": "R programming Stuff",
         "company": "Nimble Clincal Research",
         "url": "https://nimble-cr.com/",
-        "startDate": "2021",
-        "endDate": "2023",
+        "startDate": "Oct, 2021",
+        "endDate": "May, 2023",
         "techs": ["FastAPI", "R Programming", "Docker", "Django"]
     },
 ]
@@ -24,8 +42,8 @@ export const edu = [
     {
         "title": "Computer Engineering",
         "degree": "M.S. Engineering",
-        "startDate": "2024",
-        "endDate": "2026",
+        "startDate": "Aug, 2024",
+        "endDate": "May, 2026",
         "university": "New York University",
         "gpa": "4.0",
         "url": "https://www.nyu.edu/",
@@ -33,8 +51,8 @@ export const edu = [
     {
         "title": "Computer Engineering",
         "degree": "B.E. Engineering",
-        "startDate": "2017",
-        "endDate": "2022",
+        "startDate": "Oct, 2017",
+        "endDate": "Mar, 2022",
         "university": "Kathmandu University",
         "gpa": "3.83",
         "url": "https://ku.edu.np/",
